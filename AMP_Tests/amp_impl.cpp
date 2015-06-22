ced9407dad1bfe8ad690ec64a33ac16075cb1bc0
mcopik <mcopik@gmail.com>
2015-06-22 03:52:03 +0200

    Add first AMP example

---
+++ AMP_Tests/amp_impl.cpp
@@ -0,0 +1,21 @@
+#include <amp.h>
+#include <iostream>
+#include "amp_template.h"
+#include "lambda.h"
+
+using namespace Concurrency;
+
+void call_amp(std::vector<int> & v, size_t count, const function< int(double) > & f)
+{
+	array_view<int> av(count, v);
+
+	auto lambda = [=](int v)  -> double { return 1;};
+
+	parallel_for_each(av.get_extent(), [=](index<1> idx) restrict(amp) {
+    		av[idx] += lambda(0); //Lambda::get_lambda()(0);
+ 	});
+
+	for(unsigned int i = 0; i < av.get_extent().size(); i++)
+	    std::cout << static_cast<char>(av(i));
+}
+

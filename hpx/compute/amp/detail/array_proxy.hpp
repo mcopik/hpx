ed74e1383a2247520f4966f8215bdb55de025e11
Marcin Copik <mcopik@gmail.com>
2016-06-13 18:47:02 -0500

    Buffer proxy for AMP arrray

---
+++ hpx/compute/amp/detail/array_proxy.hpp
@@ -0,0 +1,64 @@
+//  Copyright (c) 2016 Marcin Copik
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+
+#ifndef HPX_BUFFER_PROXY_HPP
+#define HPX_BUFFER_PROXY_HPP
+
+#if defined(HPX_HAVE_AMP)
+
+namespace hpx { namespace compute { namespace amp
+{
+    template <typename T>
+    class buffer_proxy
+    {
+        typedef Concurrency::array<T, 1> buffer;
+        typedef std::size_t size_type;
+    public:
+        buffer_proxy(buffer * device_buffer) HPX_NOEXCEPT
+            : device_buffer_(device_buffer), p_(device_buffer.data())
+        {}
+
+        buffer_proxy(buffer * device_buffer, T * p) HPX_NOEXCEPT
+            : device_buffer_(device_buffer), p_(p)
+        {}
+
+        buffer_proxy(buffer_proxy const& other)
+            : device_buffer_(other.device_buffer_), p_(other.p_)
+        {}
+
+        operator T() const
+        {
+            return access_target::read(*target_, p_);
+        }
+
+        T& operator*() const
+        {
+            return *p_;
+        }
+
+        buffer_proxy<T> operator+(size_type pos)
+        {
+            return buffer_proxy(device_buffer_, p_ + pos);
+        }
+
+        T& operator++()
+        {
+            ++p_;
+            return *this;
+        }
+
+        T& operator--()
+        {
+            --p_;
+            return *this;
+        }
+
+    private:
+        buffer* device_obj;
+        T * p_;
+    };
+}}}
+
+#endif //HPX_BUFFER_PROXY_HPP

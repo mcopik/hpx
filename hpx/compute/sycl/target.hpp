d146e82480bc7f768ec206ce1a9ae921b4fec2f3
mcopik <mcopik@gmail.com>
2016-11-26 16:45:50 +0100

    Rewrite parts of target implementation

--- hpx/compute/sycl/target.hpp
+++ hpx/compute/sycl/target.hpp
@@ -17,6 +17,7 @@
 #include <hpx/lcos/local/spinlock.hpp>
 #include <hpx/config/warnings_prefix.hpp>
 #include <hpx/compute/sycl/config.hpp>
+#include <hpx/compute/sycl/get_targets.hpp>
 
 #include <mutex>
 #include <string>
@@ -54,6 +55,11 @@ namespace hpx { namespace compute { namespace sycl
             native_handle_type& operator=(const native_handle_type & rhs) HPX_NOEXCEPT;
             native_handle_type& operator=(native_handle_type && rhs) HPX_NOEXCEPT;
 
+            int get_device_idx() const HPX_NOEXCEPT
+            {
+                return device_idx_;
+            }
+
             device_t & get_device() const HPX_NOEXCEPT
             {
                 return device_;
@@ -64,11 +70,6 @@ namespace hpx { namespace compute { namespace sycl
                 return queue_;
             }
 
-            hpx::id_type const& get_locality() const HPX_NOEXCEPT
-            {
-                return locality_;
-            }
-
         private:
             friend struct target;
 
@@ -76,24 +77,31 @@ namespace hpx { namespace compute { namespace sycl
             int device_idx_;
             mutable device_t device_;
             mutable queue_t queue_;
-            hpx::id_type locality_;
         };
 
         // Constructs default target
-        target() HPX_NOEXCEPT {}
+        target() HPX_NOEXCEPT
+          : handle_(),
+            locality_(hpx::find_here())
+        {}
 
         // Constructs target from a given device ID
         explicit target(int device)
-          : handle_(device)
+          : handle_(device),
+            locality_(hpx::find_here())
         {}
 
         target(const target & rhs) HPX_NOEXCEPT
-          : handle_(rhs.handle_)
+          : handle_(rhs.handle_),
+            locality_(rhs.locality_)
         {}
 
         target(target && rhs) HPX_NOEXCEPT
-          : handle_(std::move(rhs.handle_))
-        {}
+          : handle_(std::move(rhs.handle_)),
+            locality_(rhs.locality_)
+        {
+            rhs.locality_ = hpx::invalid_id;
+        }
 
         target& operator=(const target & rhs) HPX_NOEXCEPT
         {
@@ -109,6 +117,7 @@ namespace hpx { namespace compute { namespace sycl
             if(&rhs != this)
             {
                 handle_ = std::move(rhs.handle_);
+                rhs.locality_ = hpx::invalid_id;
             }
             return *this;
         }
@@ -118,19 +127,46 @@ namespace hpx { namespace compute { namespace sycl
             return handle_;
         }
 
+        hpx::id_type const& get_locality() const HPX_NOEXCEPT
+        {
+            return locality_;
+        }
+
+        static std::vector<target> get_local_targets()
+        {
+            return sycl::get_local_targets();
+        }
+
+        static hpx::future<std::vector<target> >
+            get_targets(hpx::id_type const& locality)
+        {
+            //FIXME: infinite recursion, call the true function
+            return sycl::get_targets(locality);
+        }
+
+        friend bool operator==(target const& lhs, target const& rhs)
+        {
+            return lhs.handle_.get_device_idx() == rhs.handle_.get_device_idx() &&
+                lhs.locality_ == rhs.locality_;
+        }
+
         void synchronize() const;
 
         hpx::future<void> get_future() const;
+
+        static void async_handler(cl::sycl::exception_list el);
+
     private:
         friend class hpx::serialization::access;
 
         template <typename Archive>
         void serialize(Archive& ar, const unsigned int version)
         {
-            ar & handle_.device_idx_ & handle_.locality_;
+            ar & handle_.device_idx_ & locality_;
         }
 
         native_handle_type handle_;
+        hpx::id_type locality_;
     };
 
     HPX_API_EXPORT target& get_default_target();
